14ce077385361203ad37cae5f49ac03c4514128f
NasirSultan <rainasirsultan123@gmail.com>
2025-05-27 08:32:06 +0500

    s

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -43,7 +43,7 @@ const Layout = ({ children }) => {
       </div>
 
       {/* Content */}
-      <main className="flex-1 md:overflow-y-auto p-4">
+      <main className="flex-1">
         {children}
       </main>
 
