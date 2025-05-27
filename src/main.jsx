5dbcf900752d064b04b163e68e7b2e00857fcccc
NasirSultan <rainasirsultan123@gmail.com>
2025-05-27 07:03:28 +0500

    d

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import './index.css';
-import './app.css';
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
